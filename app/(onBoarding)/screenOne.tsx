4bb69f77b671eaaa35a26af1fa5a608a45453c04
Abdelrahman-Dev9 <abdoelnagar198@gmail.com>
2025-11-10 11:22:59 +0200

    create file structure

---
+++ app/(onBoarding)/screenOne.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ScreenOne = () => {
+  return (
+    <View>
+      <Text>ScreenOne</Text>
+    </View>
+  );
+};
+
+export default ScreenOne;
