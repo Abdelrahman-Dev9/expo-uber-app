9da52af7cba2fa697645688d93fffa12445524f2
Abdelrahman-Dev9 <abdoelnagar198@gmail.com>
2025-11-14 19:31:09 +0200

    create the chat screen

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Chat = () => {
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
+    <SafeAreaView className="items-center justify-center flex-1">
+      <View>
+        <Image
+          source={require("@/assets/images/message.png")}
+          className="w-[264px] h-[115px]"
+        />
+        <Text className="text-[28px] font-semibold mt-10">
+          No Messages, yet.
+        </Text>
+        <Text className="text-[17px] mt-4 text-[#858585]">
+          No messages in your inbox, yet!
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
